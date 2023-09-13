dbb4cc4d14d8a3bb751396bf9c8ea1cd56037685
emrahssamdan <emrahsamdan@gmail.com>
2023-09-14 01:23:08 +0300

    turn it into resend parser

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
-import { http } from "@ampt/sdk";
+import { http,params } from "@ampt/sdk";
 import express, { Router } from "express";
-
+import {data} from "@ampt/data";
+import { Resend } from 'resend';
+const resend = new Resend(params("RESEND_API_KEY"));
 const app = express();
-
+app.use(express.json());
 const api = Router();
 
-api.get("/hello", (req, res) => {
-  return res.status(200).send({ message: "Hello from the public api!" });
-});
-
-api.get("/greet/:name", (req, res) => {
-  const { name } = req.params;
-
-  if (!name) {
-    return res.status(400).send({ message: "Missing route param for `name`!" });
-  }
-
-  return res.status(200).send({ message: `Hello ${name}!` });
-});
-
-api.post("/submit", async (req, res) => {
+api.post("/signup", async (req, res) => {
+  const { email, firstName, lastName } = req.body || {};
+ 
+  const result = data.set(`user_${email}`, {
+    email, 
+    firstName,
+    lastName
+  });
   return res.status(200).send({
     body: req.body,
-    message: "You just posted data",
+    message: "Signup successful!",
+  });
+});
+
+data.on("created:user_*", async(event) => {
+  const record = event.item.value;
+  resend.emails.send({
+    from: 'notifications@emrahsamdan.com',
+    to: record.email,
+    subject: 'Hello ' + record.firstName + " with Resend!",
+    html: '<p>Welcome to my tiny SaaS <strong>' + record.firstName + ' ' + record.lastName + '</strong>!</p>'
+  });
+});
+
+api.post("/webhook", async (req, res) => {
+  const payload = req.body || {};
+  if(payload.type == 'email.bounced') {
+    payload.data.to.forEach(element => {
+      data.remove('user_' + element);
+    });
+  }
+   
+  return res.status(200).send({
+    message: "Webhook handled!",
   });
 });
 
